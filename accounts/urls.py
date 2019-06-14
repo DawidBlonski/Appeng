18b3912a0da9122fcca1b6584b9fae0a7d3f478f
Dawid <davidblonsky88@gmail.com>
2019-06-14 18:00:44 +0200

    added requirements.txt

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from accounts import views
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import LoginView,LogoutView
 
 app_name = "accounts"
 urlpatterns = [
-    path("logout/", logout,{'next_page': 'appeng:home'},name="logout"),
-    path("login/", login, {"template_name": "login.html"}, name="login"),
+    path("logout/", LogoutView,{'next_page': 'appeng:home'},name="logout"),
+    path("login/", LoginView, {"template_name": "login.html"}, name="login"),
     path("register/", views.register, name="register"),
 ]
